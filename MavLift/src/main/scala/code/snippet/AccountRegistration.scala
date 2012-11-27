2fa3579fae47ab141e7041db03f62a3010047ee7
Ayoub BENALI <ayoub@tesobe.com>
2012-11-27 18:32:29 +0100

    added the encryption of the PIN code of the bank account

--- MavLift/src/main/scala/code/snippet/AccountRegistration.scala
+++ MavLift/src/main/scala/code/snippet/AccountRegistration.scala
@@ -7,36 +7,46 @@ import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
 import net.liftweb.http.SHtml 
 import net.liftweb.http.S
+import net.liftweb.util.Props
 import code.pgp.PgpEncryption
 
 class AccountRegistration {
 	def renderForm = {
 		OBPUser.currentUser match {
 			case Full(user) => {
-				var banks : List[(String, String)] = HostedBank.findAll.map(t => (t.id.get.toString,t.name.get)) 
-				banks ::= ("0","Choose a Bank")
+				var banks : List[(String, String)] = HostedBank.findAll.map(t => (t.name.get,t.name.get)) 
+				banks ::= ("","Choose a Bank")
 				
-				var bankId="0"
+				var bankName      = ""
 				var accountNumber = ""
-				var accountPIN = ""
+				var accountPIN    = ""
 
 				def check() = 
-					if( !accountNumber.isEmpty & !accountPIN.isEmpty & bankId!="0")
-					{
-						println("--> all the parameters are corrects")
-//						PgpEncryption.encryptToFile()
-						//encryte the file
-					}
+					if( !accountNumber.isEmpty & !accountPIN.isEmpty & !bankName.isEmpty)
+						for{
+								publicKey 						<- Props.get("publicKeyPath")
+								outputFilesDirectory 	<- Props.get("outputFilesDirectory")
+						}yield tryo{
+								PgpEncryption.encryptToFile(	
+									accountPIN,
+									publicKey,
+									outputFilesDirectory+"/"+bankName+"-"+accountNumber+"-"+user.emailAddress+".pin")
+							} match {
+								case Full(encryptedPin) => {
+									//rederect the user to the final page
+								}
+								case _ => //redirect the user to the problem page
+							}
 					else
 					{
-						if(bankId=="0")
+						if(bankName.isEmpty)
 							S.error("bankError","Bank not selected ! ")							
 						if(accountNumber.isEmpty)
 							S.error("accountNumberError","Account Number Empty ! ")
 						if(accountPIN.isEmpty)
 							S.error("accountPINError","Account PIN Empty ! ")							
 					}
-				"#bankListCol * "#> SHtml.select(banks,Full(bankId),bankId = _,("id","bankList")) &
+				"#bankListCol * "#> SHtml.select(banks,Full(bankName),bankName = _,("id","bankList")) &
 				"#accountNumberCol *" #> SHtml.text(accountNumber,accountNumber = _,("id","accountNumber")) &
 				"#accountPINCol *" #> SHtml.password(accountPIN,accountPIN = _,("id","accountPIN")) &		
 				"type=submit" #> SHtml.onSubmitUnit(check) &
