67621dc382b0f4349633624d46db7977c48cb904
Ayoub BENALI <ayoub@tesobe.com>
2012-11-27 14:50:55 +0100

    added a form check

--- MavLift/src/main/scala/code/snippet/AccountRegistration.scala
+++ MavLift/src/main/scala/code/snippet/AccountRegistration.scala
@@ -6,17 +6,39 @@ import scala.xml.NodeSeq
 import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
 import net.liftweb.http.SHtml 
+import net.liftweb.http.S
 
-class AccountRegistration{
-	
-	def render = {
+class AccountRegistration {
+	def renderForm = {
 		OBPUser.currentUser match {
 			case Full(user) => {
-				var banks : List[(String, String)] = HostedBank.findAll.map(t => (t.id.get.toString,t.name.get))
-				banks ::= ("0","--> Choose a Bank")
-				def bankId(id :String) = id
-				"#bankList" #> SHtml.select(banks,Empty,bankId _) &
-				"#loginMsg * " #> ""	
+				var banks : List[(String, String)] = HostedBank.findAll.map(t => (t.id.get.toString,t.name.get)) 
+				banks ::= ("0","Choose a Bank")
+				
+				var bankId="0"
+				var accountNumber = ""
+				var accountPIN = ""
+
+				def check() = 
+					if( !accountNumber.isEmpty & !accountPIN.isEmpty & bankId!="0")
+					{
+						println("--> all the parameters are corrects")
+						//encryte the file
+					}
+					else
+					{
+						if(bankId=="0")
+							S.error("bankError","Bank not selected ! ")							
+						if(accountNumber.isEmpty)
+							S.error("accountNumberError","Account Number Empty ! ")
+						if(accountPIN.isEmpty)
+							S.error("accountPINError","Account PIN Empty ! ")							
+					}
+				"#bankListCol * "#> SHtml.select(banks,Full(bankId),bankId = _,("id","bankList")) &
+				"#accountNumberCol *" #> SHtml.text(accountNumber,accountNumber = _,("id","accountNumber")) &
+				"#accountPINCol *" #> SHtml.password(accountPIN,accountPIN = _,("id","accountPIN")) &		
+				"type=submit" #> SHtml.onSubmitUnit(check) &
+				"#loginMsg" #> NodeSeq.Empty	
 			}
 			case _ => 
 			"#submitAccount" #> NodeSeq.Empty &
