468fb7971bd4ff3030c50277bd03d8a6586bfb6e
Ayoub BENALI <ayoub@tesobe.com>
2013-01-14 17:46:35 +0100

    first step to add a feedback button

--- MavLift/src/main/webapp/media/js/website.js
+++ MavLift/src/main/webapp/media/js/website.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-
+	$("#feedBack").toggle(
+		function(){
+			$("#feedBack").animate({left:"0px"});
+				return false;
+		},
+		function(){
+			$("#feedBack").animate({left:"-195px"});	
+			return false;
+		})
 });
