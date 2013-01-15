0c5ed8efc900c56f2942d2bcf4390d6a404c4638
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2013-01-15 15:57:26 +0100

    Try something

--- MavLift/src/main/webapp/media/js/website.js
+++ MavLift/src/main/webapp/media/js/website.js
@@ -14,24 +14,25 @@ $(document).ready(function() {
     $.post($(this).attr("action"), $(this).serialize(), function(data) {
       console.log("it works");
       console.log(data);
+      $('#feedback-angel').html("<p>Thank you!</p>");
     });
   });
 
   $('#feedback-demon').submit(function(e) {
-    e.preventDefault();
     $.post($(this).attr("action"), $(this).serialize(), function(data) {
       console.log("it works");
       console.log(data);
+      $('#feedback-demon').html("<p>Thank you!</p>");
     });
+    return false;
   });
 
-  $("#feedback-idea .submit-button").click(function() {
-    $('#feedback-idea').submit(function(e) {
-      e.preventDefault();
-      $.post($(this).attr("action"), $(this).serialize(), function(data) {
-        console.log("it works"),
-        console.log(data);
-      });
+  $('#feedback-idea').submit(function(e) {
+  e.preventDefault();
+    $.post($(this).attr("action"), $(this).serialize(), function(data) {
+      console.log("it works"),
+      console.log(data);
+      $('#feedback-idea').html("<p>Thank you!</p>");
     });
   });
 });
