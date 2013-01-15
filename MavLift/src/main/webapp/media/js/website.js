1b2fd3d7b68d881a8e7e92af48dc0986ca9249b8
Ayoub BENALI <ayoub@tesobe.com>
2013-01-15 15:14:07 +0100

    work in progress

--- MavLift/src/main/webapp/media/js/website.js
+++ MavLift/src/main/webapp/media/js/website.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
-	$("#feedBack").toggle(
-		function(){
-			$("#feedBack").animate({left:"0px"});
-				return false;
-		},
-		function(){
-			$("#feedBack").animate({left:"-195px"});	
-			return false;
-		})
+    $(".feedback-slider").toggle(
+        function(){
+            $("#feedBack").animate({left:"0px"});
+                return false;
+        },
+        function(){
+            $("#feedBack").animate({left:"-195px"});    
+            return false;
+        });
+	$('#feedback-angel').submit(function(e) {
+	  e.preventDefault();
+	  $.post($(this).attr("action"), $(this).serialize(), function(data) {
+	    console.log("it works");
+	    console.log(data);
+	  });
+	});
+
+	$('#feedback-demon').submit(function(e) {
+	  e.preventDefault();
+	  $.post($(this).attr("action"), $(this).serialize(), function(data) {
+	    console.log("it works");
+	    console.log(data);
+	  });
+	});
+
+	$('#feedback-idea').submit(function(e) {
+	  e.preventDefault();
+	  $.post($(this).attr("action"), $(this).serialize(), function(data) {
+	    console.log("it works"),
+	    console.log(data);
+	  });
 });
+});    
