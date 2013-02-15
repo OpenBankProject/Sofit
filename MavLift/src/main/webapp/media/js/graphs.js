2ca2d0332fd978dad872edbf0df031fbb9040d04
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2013-02-15 11:47:29 +0100

    Refactor the bar chart

--- MavLift/src/main/webapp/media/js/graphs.js
+++ MavLift/src/main/webapp/media/js/graphs.js
@@ -1,57 +1,142 @@
 d3.json("obp/v1.0/metrics/demo-bar", function(data) {
-    var content = d3.select("#content");
-    var bar_chart = content.append("div").attr("class", "bar-chart");
+    data = data.stats;
+    //maximum of data you want to use
+    var content = d3.select("#content"),
+        data_max = d3.max(data).amount,
+        //number of tickmarks to use
+        num_ticks = 20,
+        //margins
+        left_margin = 330,
+        right_margin = 330,
+        top_margin = 30,
+        bottom_margin = 0;
 
-    // Include headings ffor the Graph
-    content.insert("h2", ".bar-chart").text("Bar Graph");
-    content.insert("h3", ".bar-chart").text("Most used API calls");
+    var w = 1222,                        //width
+        h = 600,                        //height
+        color = function(id) { return '#00b3dc';};
 
-    // Bind only the nested data to the dataset
-    var datasets = data.stats;
-    var data_length = datasets.length;
+    var x = d3.scale.linear()
+        .domain([0, data_max])
+        .range([0, w - (left_margin + right_margin) ]),
+        y = d3.scale.ordinal()
+        .domain(d3.range(data.length))
+        .rangeBands([bottom_margin, h - top_margin], 0.5);
 
-    //Width and height
-    var w = 1000;
-    var h = 500;
-    var barPadding = 2;
-    var margin = 20;
 
-    //Create SVG element
-    var svg = content
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h)
-                .attr("class", "svg-bar-chart");
+    var chart_top = h - y.rangeBand()/2 - top_margin;
+    var chart_bottom = bottom_margin + y.rangeBand()/2;
+    var chart_left = left_margin;
+    var chart_right = w - right_margin;
 
-    var xScale = d3.scale.linear().domain([0, d3.max(data.stats).amount]).range([0, w]);
+    /*
+     *  Setup the SVG element and position it
+     */
+    var vis = content
+        .append("svg:svg")
+            .attr("width", w)
+            .attr("height", h)
+        .append("svg:g")
+            .attr("id", "barchart")
+            .attr("class", "barchart");
 
-    svg.selectAll("rect")
-            .data(datasets)
-        .enter().append("rect")
-            .attr("y", function(d, i) {
-                return i * (h / data_length);
-            })
-            .attr("x", margin)
-            .attr("height", h / data_length - barPadding)
-            .attr("width", function(d) {
-                return xScale(d.amount);
-            })
-            .attr("fill", function(d) {
-                return "rgb(243, 249, " + (d.amount * 10 + 200) + ")";
+
+    //Ticks
+    var rules = vis.selectAll("g.rule")
+        .data(x.ticks(num_ticks))
+    .enter()
+        .append("svg:g")
+        .attr("transform", function(d)
+                {
+                return "translate(" + (chart_left + x(d)) + ")";});
+    rules.append("svg:line")
+        .attr("class", "tick")
+        .attr("y1", chart_top)
+        .attr("y2", chart_top + 4)
+        .attr("stroke", "black");
+
+    rules.append("svg:text")
+        .attr("class", "tick_label")
+        .attr("text-anchor", "middle")
+        .attr("y", chart_top)
+        .text(function(d)
+        {
+        return d;
+        });
+    var bbox = vis.selectAll(".tick_label").node().getBBox();
+    vis.selectAll(".tick_label")
+    .attr("transform", function(d)
+            {
+            return "translate(0," + (bbox.height) + ")";
             });
 
-        svg.selectAll("text")
-                .data(datasets)
-           .enter().append("text")
-               .text(function(d) {
-                    return d.amount;
-               })
-               .attr("y", function(d, i) {
-                    return i * (h / data_length) + (h / data_length - barPadding) / 1.5;
-               })
-               .attr("x", function(d) {
-                    return margin * 2;
-               });
+    var bars = vis.selectAll("g.bar")
+        .data(data)
+    .enter()
+        .append("svg:g")
+            .attr("class", "bar")
+            .attr("transform", function(d, i) {
+                    return "translate(0, " + y(i) + ")";
+            });
+
+    bars.append("svg:rect")
+        .attr("x", right_margin)
+        .attr("width", function(d) {
+                return (x(d.amount));
+                })
+        .attr("height", y.rangeBand())
+        .attr("fill", color(0))
+        .attr("stroke", color(0));
+
+
+    //Labels
+    var labels = vis.selectAll("g.bar")
+        .append("svg:a").attr("width", 200).append("svg:text")
+            .attr("class", "label")
+            .attr("x", 20)
+            .attr("text-anchor", "right")
+            .text(function(d) {
+                    return d.url;
+                    });
+
+    bbox = labels.node().getBBox();
+    vis.selectAll(".label")
+        .attr("transform", function(d) {
+                return "translate(0, " + (y.rangeBand()/2 + bbox.height / 4 ) + ")";
+                });
+
+
+    labels = vis.selectAll("g.bar")
+        .append("svg:text")
+            .attr("class", "value")
+            .attr("x", function(d) {
+                return x(d.amount) + right_margin + 10;
+            })
+            .attr("text-anchor", "left")
+            .text(function(d) {
+                return "" + d.amount;
+            });
+
+    bbox = labels.node().getBBox();
+    vis.selectAll(".value")
+        .attr("transform", function(d) {
+            return "translate(0, " + (y.rangeBand()/2 + bbox.height/4) + ")";
+        });
+
+    //Axes
+    vis.append("svg:line")
+        .attr("class", "axes")
+        .attr("x1", chart_left)
+        .attr("x2", chart_left)
+        .attr("y1", chart_bottom)
+        .attr("y2", chart_top)
+        .attr("stroke", "black");
+     vis.append("svg:line")
+        .attr("class", "axes")
+        .attr("x1", chart_left)
+        .attr("x2", chart_right)
+        .attr("y1", chart_top)
+        .attr("y2", chart_top)
+        .attr("stroke", "black");
 });
 
 
