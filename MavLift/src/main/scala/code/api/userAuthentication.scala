8b62f4979d7046e51c1d383318e07c17cc135fbb
Ayoub BENALI <ayoub@tesobe.com>
2013-03-01 11:40:04 +0100

    added the application information API call, required for the bank mockup module

--- MavLift/src/main/scala/code/api/userAuthentication.scala
+++ MavLift/src/main/scala/code/api/userAuthentication.scala
@@ -46,8 +46,14 @@ import code.model.TokenType
 case class ErrorMessage(
   error : String
 )
+
 case class TokenValidity(
   isValid : Boolean
+)
+
+case class ApplicationInformation(
+  name : String,
+  callbackURL : String
 )
 
 /**
@@ -59,6 +65,7 @@ object BankMockAPI extends RestHelper with Loggable {
 
   implicit def errorToJson(error: ErrorMessage): JValue = Extraction.decompose(error)
   implicit def TokenValidityToJson(msg: TokenValidity): JValue = Extraction.decompose(msg)
+  implicit def applicationInfoToJson(info: ApplicationInformation): JValue = Extraction.decompose(info)
 
   //extract and compare the sent key with the local one (shared secret)
   def isValidKey : Boolean = {
@@ -77,6 +84,34 @@ object BankMockAPI extends RestHelper with Loggable {
           case Full(token) => {
             Token.find(By(Token.key,token), By(Token.tokenType,TokenType.Request)) match {
               case Full(tkn) => JsonResponse(TokenValidity(tkn.isValid), Nil, Nil, 200)
+              case _ => JsonResponse(ErrorMessage("invalid or expired OAuth token"), Nil, Nil, 401)
+            }
+          }
+          case _ => JsonResponse(ErrorMessage("No OAuth token"), Nil, Nil, 401)
+        }
+      else
+        JsonResponse(ErrorMessage("No key found or wrong key"), Nil, Nil, 401)
+    }
+
+    case "application-information" :: Nil JsonGet _ => {
+      if(isValidKey)
+        S.param("oauth_token") match {
+          case Full(token) => {
+            Token.find(By(Token.key,token), By(Token.tokenType,TokenType.Request)) match {
+              case Full(tkn) =>
+                if(tkn.isValid)
+                  tkn.consumerId.foreign match {
+                    case Full(app) => {
+                      val applicationInfo = ApplicationInformation(
+                        name  = app.name,
+                        callbackURL = tkn.callbackURL.get
+                      )
+                      JsonResponse(applicationInfo, Nil, Nil, 200)
+                    }
+                    case _ => JsonResponse(ErrorMessage("Application not found"), Nil, Nil, 400)
+                  }
+                else
+                  JsonResponse(ErrorMessage("invalid or expired OAuth token"), Nil, Nil, 401)
               case _ => JsonResponse(ErrorMessage("invalid or expired OAuth token"), Nil, Nil, 401)
             }
           }
