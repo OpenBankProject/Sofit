3814079b538b11a9dc6fd635fc339e9df096a49d
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2013-04-12 17:36:41 +0200

    Fix settings variable and change margin to padding to increase the elements event area

--- MavLift/src/main/webapp/media/js/website.js
+++ MavLift/src/main/webapp/media/js/website.js
@@ -1,6 +1,6 @@
 $(function() {
     $("body").polarize({
-        "direction": "right",
+        "position": "right",
         "images_dir": "/media/images/",
         "default_topic_url": "http://polarize.it/polarize/socialfinanceapp_55868373368"
     });
