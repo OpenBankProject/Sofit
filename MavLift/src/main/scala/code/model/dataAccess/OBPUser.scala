00cde6b64d0b22322e4d8a9a240f3d2c072ac70c
Everett Sochowski <everett@tesobe.com>
2013-04-15 15:06:20 +0200

    Do oauth login for  newly registered users

--- MavLift/src/main/scala/code/model/dataAccess/OBPUser.scala
+++ MavLift/src/main/scala/code/model/dataAccess/OBPUser.scala
@@ -48,6 +48,7 @@ import net.liftweb.util.Helpers._
 import org.bson.types.ObjectId
 import com.mongodb.DBObject
 import net.liftweb.json.JsonAST.JObject
+import code.lib.OAuthClient
 
 
 /**
@@ -186,7 +187,35 @@ object OBPUser extends OBPUser with MetaMegaProtoUser[OBPUser]{
     loginReferer.remove()
     ret
   }
+  
+  /**
+   * Overriden to go through the oauth authentication after a user signs up and gets logged in. This is a temporary
+   * thing until the Social Finance app is split from the API.
+   */
+  override def signup = {
+    val theUser: TheUserType = mutateUserOnSignup(createNewUserInstance())
+    val theName = signUpPath.mkString("")
 
+    def testSignup() {
+      validateSignup(theUser) match {
+        case Nil =>
+          actionsAfterSignup(theUser, () => {
+            //Redirect to oauth. This will need to be revisited when the Social Finance is unlinked from the API.
+            val provider = OAuthClient.defaultProvider
+            S.redirectTo(OAuthClient.getAuthUrl(provider))
+          })
+
+        case xs => S.error(xs) ; signupFunc(Full(innerSignup _))
+      }
+    }
+
+    def innerSignup = bind("user",
+                           signupXhtml(theUser),
+                           "submit" -> SHtml.submit(S.??("sign.up"), testSignup _))
+
+    innerSignup
+  }
+  
   override def loginXhtml = {
     import net.liftweb.http.TemplateFinder
     import net.liftweb.http.js.JsCmds.Noop
