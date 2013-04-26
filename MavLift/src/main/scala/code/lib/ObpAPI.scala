7dffa6ce3d9645ccce35b6c32737c37d32ec0d08
Everett Sochowski <everett@tesobe.com>
2013-04-26 14:10:33 +0200

    Added some case classes to extract the obp json into

--- MavLift/src/main/scala/code/lib/ObpAPI.scala
+++ MavLift/src/main/scala/code/lib/ObpAPI.scala
@@ -61,3 +61,38 @@ object ObpGet {
     }
   }
 }
+
+object ObpJson {
+  import net.liftweb.json._
+  implicit val formats = DefaultFormats
+  case class BanksJson(banks : Option[List[BankJson]])
+  case class BankJson(id: Option[String], 
+		  		  short_name: Option[String],
+		  		  full_name: Option[String],
+		  		  logo: Option[String],
+		  		  website: Option[String])
+		  		  
+  case class AccountOwnerJson(user_id: Option[String],
+		  				  user_provider: Option[String],
+		  				  display_name: Option[String])
+  
+  case class AccountBalanceJson(currency: Option[String],
+		  					amount: Option[String])		  	
+	
+  case class ViewJson(id: Option[String],
+		  		  short_name: Option[String],
+		  		  description: Option[String],
+		  		  is_public: Option[Boolean])
+		  		  
+  case class AccountJson(id: Option[String],
+		  			 number: Option[String],
+		  			 owners: Option[List[AccountOwnerJson]],
+		  			 `type`: Option[String],
+		  			 balance: Option[AccountBalanceJson],
+		  			 IBAN : Option[String],
+		  			 views_available: Option[List[ViewJson]])
+		  			 
+  case class BarebonesAccountJson(id: Option[String],
+		  						  label: Option[String],
+		  						  views_available: Option[List[ViewJson]])
+}
