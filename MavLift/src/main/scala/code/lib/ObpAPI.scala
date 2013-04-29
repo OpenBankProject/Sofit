a6737b35eb42e836b55f830db03c4660d19fd7fd
Everett Sochowski <everett@tesobe.com>
2013-04-29 13:38:40 +0200

    Added more case classes for obp api json

--- MavLift/src/main/scala/code/lib/ObpAPI.scala
+++ MavLift/src/main/scala/code/lib/ObpAPI.scala
@@ -16,6 +16,7 @@ import net.liftweb.common.Failure
 import java.io.BufferedReader
 import net.liftweb.util.Helpers._
 import java.io.InputStreamReader
+import java.util.Date
 
 object ObpGet {
   //Ah, dispatch does have oauth support. It would be nicer to use dispatch! -E.S.
@@ -72,7 +73,7 @@ object ObpJson {
 		  		  logo: Option[String],
 		  		  website: Option[String])
 		  		  
-  case class AccountOwnerJson(user_id: Option[String],
+  case class UserJson(user_id: Option[String],
 		  				  user_provider: Option[String],
 		  				  display_name: Option[String])
   
@@ -86,7 +87,7 @@ object ObpJson {
 		  		  
   case class AccountJson(id: Option[String],
 		  			 number: Option[String],
-		  			 owners: Option[List[AccountOwnerJson]],
+		  			 owners: Option[List[UserJson]],
 		  			 `type`: Option[String],
 		  			 balance: Option[AccountBalanceJson],
 		  			 IBAN : Option[String],
@@ -95,4 +96,77 @@ object ObpJson {
   case class BarebonesAccountJson(id: Option[String],
 		  						  label: Option[String],
 		  						  views_available: Option[List[ViewJson]])
+		  						  
+  case class HolderJson(name: Option[String],
+		  				is_alias : Option[Boolean])
+		  				
+  //TODO: Can this go with BankJson?
+  case class LightBankJson(national_identifier: Option[String],
+		  			  	   name: Option[String])
+  
+  case class ThisAccountJson(holders: Option[List[HolderJson]],
+		  					 number: Option[String],
+		  					 kind: Option[String],
+		  					 IBAN: Option[String],
+		  					 bank: Option[LightBankJson])
+  
+  case class LocationJson(latitude: Option[Double],
+		  						   longitude: Option[Double],
+		  						   date: Option[Date], //TODO: Check if the default date formatter is okay
+		  						   user: Option[UserJson])
+  
+  case class OtherAccountMetadataJson(public_alias: Option[String],
+		  							  private_alias: Option[String],
+		  							  more_info: Option[String],
+		  							  URL: Option[String],
+		  							  image_URL: Option[String],
+		  							  open_corporates_URL: Option[String],
+		  							  corporate_location: Option[LocationJson],
+		  							  physical_location: Option[LocationJson])		  					 
+		  					 
+  //TODO: Why can't an other account have more than one holder?	  					 
+  case class OtherAccountJson(holder: Option[HolderJson],
+		  					  number: Option[String],
+		  					  kind: Option[String],
+		  					  IBAN: Option[String],
+		  					  bank: Option[LightBankJson],
+		  					  metadata: Option[OtherAccountMetadataJson])
+  
+  case class TransactionValueJson(currency: Option[String],
+		  						  amount: Option[String])
+		  					  
+  case class TransactionDetailsJson(`type`: Option[String],
+		  							label: Option[String],
+		  							posted: Option[Date], //TODO: Check if the default date formatter is okay
+		  							completed: Option[Date], //TODO: Check if the default date formatter is okay
+		  							new_balance: Option[AccountBalanceJson],
+		  							value: Option[TransactionValueJson])	  					  
+		  					  
+  case class TransactionCommentJson(id: Option[String],
+		  				 date: Option[Date], //TODO: Check if the default date formatter is okay
+		  				 value: Option[String],
+		  				 user: Option[UserJson],
+		  				 reply_to: Option[String])
+  
+  case class TransactionTagJson(id: Option[String],
+		  			 date: Option[Date], //TODO: Check if the default date formatter is okay
+		  			 value: Option[String],
+		  			 user: Option[UserJson])
+  
+  case class TransactionImageJson(id: Option[String],
+		  						  label: Option[String],
+		  						  URL: Option[String])
+  
+  case class TransactionMetadataJson(narrative: Option[String],
+		  							 comments: Option[List[TransactionCommentJson]],
+		  							 tags: Option[List[TransactionTagJson]],
+		  							 images: Option[List[TransactionImageJson]],
+		  							 where: Option[LocationJson])
+  
+  case class TransactionJson(uuid: Option[String],
+		  					 id: Option[String],
+		  					 this_account: Option[ThisAccountJson],
+		  					 other_account: Option[OtherAccountJson],
+		  					 details: Option[TransactionDetailsJson],
+		  					 metadata: Option[TransactionMetadataJson])
 }
