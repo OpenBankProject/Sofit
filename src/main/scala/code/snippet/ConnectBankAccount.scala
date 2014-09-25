579d48e6c926501d7ff610ae10dfcc71223b0f4b
Everett Sochowski <everett@tesobe.com>
2014-09-25 14:34:49 +0200

    Only show connect bank account link if specified to do so in the props file

--- src/main/scala/code/snippet/ConnectBankAccount.scala
+++ src/main/scala/code/snippet/ConnectBankAccount.scala
@@ -2,25 +2,28 @@ package code.snippet
 
 import code.lib.OAuthClient
 import net.liftweb.http.{S, SHtml}
-import net.liftweb.util.{PassThru, CssSel}
+import net.liftweb.util.{Props}
 import net.liftweb.util.Helpers._
-
 import scala.xml.NodeSeq
 
 class ConnectBankAccount {
 
   def connect : NodeSeq => NodeSeq = {
 
-    val currentPage = S.uri
+    val hide = "* *" #> NodeSeq.Empty
 
-    //only show something if we're on the home page
-    if(currentPage == "/" || currentPage == "/index") {
-      ".navlink [onclick+]" #> SHtml.onEvent(_ => {
-        OAuthClient.redirectToConnectBankAccount()
-      })
-    } else {
-      "* *" #> NodeSeq.Empty
+    def showIfHomepage() = {
+      val currentPage = S.uri
+      //only show something if we're on the home page
+      if(currentPage == "/" || currentPage == "/index") {
+        ".navlink [onclick+]" #> SHtml.onEvent(_ => {
+          OAuthClient.redirectToConnectBankAccount()
+        })
+      } else hide
     }
+
+    if(Props.getBool("showConnectBankAccountLink", false)) showIfHomepage()
+    else hide
 
   }
 
