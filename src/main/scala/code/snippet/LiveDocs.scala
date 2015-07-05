98cb84033b42ba1e3f44365af1d9658ea7bae669
Simon Redfern <simon@tesobe.com>
2015-07-05 10:59:29 +0200

    Live-doscs hiding and showing better

--- src/main/scala/code/snippet/LiveDocs.scala
+++ src/main/scala/code/snippet/LiveDocs.scala
@@ -28,30 +28,47 @@ Present a list of OBP resource URLs
  */
 class LiveDocs {
   def showResources = {
-    case class Resource(verb: String,
+    case class Resource(id: String,
+                        verb: String,
                         url: String,
                         description: String,
                         representation: String)
 
     val resources = List(
-      Resource("GET", "/v1.4.0/banks", "Get banks on this server", "JSON"),
-      Resource("GET", "/v1.4.0/banks/BANK_ID", "Get a particular bank identified by its ID", "JSON"),
-      Resource("GET", "/v1.4.0/banks/BANK_ID/branches", "Get branches of a certain bank", "JSON"))
+      Resource("1", "GET", "/v1.4.0/banks", "Get banks on this server", "JSON"),
+      Resource("2", "GET", "/v1.4.0/banks/BANK_ID", "Get a particular bank identified by its ID", "JSON"),
+      Resource("3", "GET", "/v1.4.0/banks/BANK_ID/branches", "Get branches of a certain bank", "JSON"))
+
+
+
+
+
+  // Notes on below. To have a $ in the resulting string use two: $$
+  // Can't escape " with \" or use triple quoted string in the string interpolation so use the replace hack
+
 
     ".resource" #> resources.map { i =>
-      ".resource-verb" #> i.verb &
-        ".resource-url" #> i.url &
-        ".resource-description" #> i.description &
-        ".resource-representation" #> i.representation
+      ".resource_verb" #> i.verb &
+      ".resource_url" #> i.url &
+      ".resource_description" #> i.description &
+      ".resource_representation" #> i.representation &
+      ".resource_url_td [id]" #> s"resource_url_td_${i.id}" &
+      ".url_caller [id]" #> s"url_caller_${i.id}" &
+      ".url_to_call [id]" #> s"url_to_call_${i.id}" &
+      ".try_me_button [onclick]" #> s"$$(DOUBLE-QUOTE#url_caller_${i.id}DOUBLE-QUOTE).fadeToggle(); $$(DOUBLE-QUOTE#url_to_call_${i.id}DOUBLE-QUOTE).val($$(DOUBLE-QUOTE#resource_url_td_${i.id}DOUBLE-QUOTE)[0].innerHTML);".replaceAll("DOUBLE-QUOTE",""""""")
     }
   }
 }
 
 
+
+
+
+
 /*
 Call an OBP URL and return the response to the browser in JSON form.
  */
-object ApiCallForm extends Loggable {
+object CallMe extends Loggable {
 
 
   def getResponse (url : String ) : String = {
@@ -83,17 +100,15 @@ object ApiCallForm extends Loggable {
 
       var urlToCall = ""
 
-      // /v1.4.0/banks
-
       def process(): JsCmd = {
         SetHtml("result", Text(getResponse(urlToCall)))
       }
 
       // form field (on the left) is bound to the variable (urlToCall)
-      // (However, updating the var here does not seeem to update the form field value)
+      // (However, updating the var here does not seem to update the form field value)
       "@url_to_call" #> text(urlToCall, s => urlToCall = s) &
         // Replace the type=submit with Javascript that makes the ajax call.
-        "type=submit" #> ajaxSubmit("Call OBP API", process)
+        "type=submit" #> ajaxSubmit("Go", process)
 
   }
 }
