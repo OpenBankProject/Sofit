d4df3e1a9f9982c9850c2484bb78e7fee8eaffc8
Marko Milić <marko@tesobe.com>
2020-08-17 15:24:39 +0200

    refactor/Bump version to 4.0.0

--- src/main/scala/code/Constant.scala
+++ src/main/scala/code/Constant.scala
@@ -1,5 +1,8 @@
 package code
 
+import net.liftweb.util.Props
+
 object Constant {
   final val CUSTOM_OWNER_VIEW_ID = "owner"
+  final val versionOfApi = Props.get("api_version").getOrElse("v4.0.0")
 }
