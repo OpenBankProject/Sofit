2f5ce62188c248e19445cfa3058b70ff4e1e546f
Marko Milić <marko@tesobe.com>
2021-12-01 16:28:05 +0100

    feature/Add property expenditure.tags

--- src/main/scala/code/snippet/CreateExpenditure.scala
+++ src/main/scala/code/snippet/CreateExpenditure.scala
@@ -3,25 +3,30 @@ package code.snippet
 import code.Constant._
 import code.lib.ObpAPI.{createOutcome, getAccount}
 import code.util.Helper.{MdcLoggable, getAccountTitle}
-import net.liftweb.http.{S, SHtml}
+import net.liftweb.common.Box
+import net.liftweb.http.{RequestVar, S, SHtml}
 import net.liftweb.http.js.JE.Call
 import net.liftweb.http.js.JsCmd
 import net.liftweb.http.js.JsCmds.SetHtml
 import net.liftweb.util.Helpers._
+import net.liftweb.util.Props
 
 import scala.xml.{NodeSeq, Text}
 
 
 class CreateExpenditure(params: List[String]) extends MdcLoggable {
+  private object tagVar extends RequestVar("")
   val bankId = params(0)
   val accountId = params(1)
   val accountJson = getAccount(bankId, accountId, CUSTOM_OWNER_VIEW_ID).openOrThrowException("Could not open accountJson")
   def accountTitle = ".account-title *" #> getAccountTitle(accountJson)
-
-  //set up ajax handlers to edit account label
+  
+  
   def addPayment(xhtml: NodeSeq): NodeSeq = {
     var outcomeDescription = ""
     var outcomeAmount = "0"
+    val listOfTags: Seq[(String, String)] = Props.get("expenditure.tags", "None,Food,Rent,Transport,Health_Insurance,Other").
+      split(",").toList.map(_.trim).map(i => i.replaceAll("_", "/")).map(i => (i,i))
 
     def process(): JsCmd = {
       logger.debug(s"CreateOutcome.addOutcome.process: edit label $outcomeDescription")
@@ -38,6 +43,7 @@ class CreateExpenditure(params: List[String]) extends MdcLoggable {
 
     (
       "@payment_description" #> SHtml.text("", s => outcomeDescription = s) &
+       "#payment_category" #> SHtml.select(listOfTags, Box!! tagVar.is, tagVar(_)) &
       "@payment_amount" #> SHtml.text("", s => outcomeAmount = s) &
         // Replace the type=submit with Javascript that makes the ajax call.
         "type=submit" #> SHtml.ajaxSubmit(S.?("button.save"), process)
